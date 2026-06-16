04beb49a7f2ea2a1592cdf1cb98261d25b301ce4
G-S-design <gabrielsoler108@gmail.com>
2026-06-15 20:48:17 -0400

    matias headshot fix?

--- script.js
+++ script.js
@@ -86,7 +86,7 @@ const siteConfig = {
       {
         name: "Matias Marjovsky",
         role: "Co-Chair",
-        image: "images/Team/Cochair/Matias.png"
+        image: "images/Team/Cochair/matias.png"
       }
     ],
     directors: [
